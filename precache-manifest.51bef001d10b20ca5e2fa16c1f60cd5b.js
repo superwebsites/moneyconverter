bda93415e971bd7e2f5abab5c9c2502dd5a8698c
Dan Golkd <dgoldring27@gmail.com>
2020-06-16 15:55:41 -0400

    Updates

---
+++ precache-manifest.51bef001d10b20ca5e2fa16c1f60cd5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f25cd1848c268903901a36d0f51a65e",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "dbc36e5cd2993b738055",
+    "url": "/currency-converter/static/css/main.45cda6f5.chunk.css"
+  },
+  {
+    "revision": "e2c2d75c2f376641478b",
+    "url": "/currency-converter/static/js/2.37ec2a6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.37ec2a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc36e5cd2993b738055",
+    "url": "/currency-converter/static/js/main.1ec1fb86.chunk.js"
+  },
+  {
+    "revision": "db273e9768f65b023b42",
+    "url": "/currency-converter/static/js/runtime-main.3ef1aaa9.js"
+  }
+]);
